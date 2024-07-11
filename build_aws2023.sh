c2c0315d7eb8758c75e764ac3557a7c43d453c77
NicoSeddiki <nseddiki@sipfront.com>
2024-07-11 11:48:07 +0200

    - added new build.sh

--- build_aws2023.sh
+++ build_aws2023.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # set up up name of virtual env
-python3.8 -m venv env
+python3.11 -m venv env
 PWD=`pwd`
 
 echo "Current pwd: "$PWD
@@ -9,10 +9,10 @@ echo "1. Activate virtual environment!"
 source $PWD/env/bin/activate
 
 echo "2. Install necessary python packages!"
-python3.8 -m pip install numpy scipy protobuf wheel
+python3.11 -m pip install numpy scipy protobuf wheel
 
 echo "3. Build visqol with bazel!"
 bazel build :visqol -c opt
 
 echo "4. Build .so visqol for python!"
-python3.8 -m pip install .
+python3.11 -m pip install .
