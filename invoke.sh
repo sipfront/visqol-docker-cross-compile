3a4c6e87ba9e47e1571b2fe63306c485edcbae31
NicoSeddiki <122271076+NicoSeddiki@users.noreply.github.com>
2024-07-11 11:40:46 +0200

    Aws linux 2023 (#2)
    * replaced python3.8 -> python3.11
    * renamed dockerfile -> Dockerfile.aws2023

--- invoke.sh
+++ invoke.sh
@@ -2,26 +2,26 @@
 
 # Specify container name
 container_name="sipfront-test"
-
 aws_linux_2=0
 
 # Pull docker image and tag it
-if ["$aws_linux_2" == 1]; then
+if [ "$aws_linux_2" == 1 ]; then
     docker pull amazonlinux:2 && docker tag amazonlinux:2 local-amazonlinux:2
+    docker build --tag="local-amazonlinux:latest" .
 else
     docker pull amazonlinux:2023 && docker tag amazonlinux:2023 local-amazonlinux:2023
+    docker build --tag="local-amazonlinux:latest" -f Dockerfile.aws2023 .
 fi
-
-
-# Build Docker image
-docker build --tag="local-amazonlinux:latest" .
 
 # Capture the container ID
 docker run --name $container_name local-amazonlinux:latest
 
 # Copy visqol build folder to working directory
 echo "Copying visqol build directory!"
-docker cp sipfront-test:/visqol/build/lib/visqol/ ./build_test/
+if [ "$aws_linux_2" == 1 ]; then
+    docker cp sipfront-test:/visqol/build/lib/visqol/ ./build_test/
+else
+    docker cp sipfront-test:/visqol/build/lib/visqol/ ./build_test_aws2023/
 
 # Remove used container
 echo "Removing container!"
