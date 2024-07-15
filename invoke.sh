b9b4ced53ddbe1bc86929489519176c726d1ae23
NicoSeddiki <nseddiki@sipfront.com>
2024-07-15 11:57:14 +0200

    updated bash script

--- invoke.sh
+++ invoke.sh
@@ -3,6 +3,28 @@
 # Specify container name
 container_name="sipfront-test"
 aws_linux_2=1
+while getopts a: flag
+do
+    case "${flag}" in
+        a) aws_linux_2=${OPTARG};;
+    esac
+done
+
+# Give directory
+dir=""
+if [ "$aws_linux_2" == 1 ]; then
+    dir="build-binaries-aws-linux-2"
+else
+    dir="build-binaries-aws-linux-2023"
+fi
+
+# Check if directory already exists
+if [[ ! -e $dir ]]; then
+    mkdir $dir
+elif [[ ! -d $dir ]]; then
+    echo "$dir already exists but is not a directory" 1>&2
+fi
+
 
 # Pull docker image and tag it
 if [ "$aws_linux_2" == 1 ]; then
@@ -18,11 +40,7 @@ docker run --name $container_name local-amazonlinux:latest
 
 # Copy visqol build folder to working directory
 echo "Copying visqol build directory!"
-if [ "$aws_linux_2" == 1 ]; then
-    docker cp sipfront-test:/visqol/build/lib/visqol/ ./build-binaries-aws2/
-else
-    docker cp sipfront-test:/visqol/build/lib/visqol/ ./build-binaries-aws2023/
-fi
+docker cp sipfront-test:/visqol/build/lib/visqol/ $dir/visqol
 
 # Remove used container
 echo "Removing container!"
