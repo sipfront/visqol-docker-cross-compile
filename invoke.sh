ad762aaa1fb9f7dae3e993f096d87e93b27a1ef1
NicoSeddiki <nseddiki@sipfront.com>
2024-07-18 07:46:18 +0200

    renamed container name

--- invoke.sh
+++ invoke.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Specify container name
-container_name="sipfront-test"
+container_name="sipfront-compile-visqol-py38-aws-linux-2"
 
-# Pull docker image and tag it 
+# Pull docker image and tag it
 docker pull amazonlinux:2 && docker tag amazonlinux:2 local-amazonlinux:2
 
 # Build Docker image
@@ -14,8 +14,8 @@ docker run --name $container_name local-amazonlinux:latest
 
 # Copy visqol build folder to working directory
 echo "Copying visqol build directory!"
-docker cp sipfront-test:/visqol/build/lib/visqol/ ./build_test/ 
+docker cp $container_name:/visqol/build/lib/visqol/ ./build_test/
 
 # Remove used container
-echo "Removing container!" 
+echo "Removing container!"
 docker rm $container_name
